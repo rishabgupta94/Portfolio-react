b696677431a663059a745fad4985f659d762ddcc
Rishab Gupta <rishabgupta98@gmail.com>
2021-01-24 12:54:51 -0400

    testing cloudfront

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,10 +13,9 @@ class Home extends Component {
         <Container>
           <Row className="align-items-center">
             <Col className="text-center">
-              <text className="name">RISHAB GUPTA</text>
+              <text className="name">TESTING</text>
               <h6 className="sub-heading">AWS Certified Solutions Architect</h6>
               <h6 className="sub-heading">Front End Web Developer</h6>
-              <h6 className="sub-heading">Testing AWS</h6>
               <Button className="get-to-know-me" variant="warning">
                 <span className="know-me-text">Get to know me</span>
               </Button>
