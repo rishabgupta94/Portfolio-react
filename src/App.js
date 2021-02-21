01b37cd9f42f39d3d7720f5bc2541a786beaaabb
Rishab Gupta <rishabgupta98@gmail.com>
2021-02-21 17:55:58 -0400

    check ip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 
 function App() {
+
+  const [mounted, setMounted] = useState(false);
+
+  if(!mounted) {
+    fetch('https://api.ipify.org?format=jsonp?callback=?', {
+      method: 'GET',
+      headers: {},
+    })
+    .then(res => {
+      return res.text()
+    }).then(ip => {
+      console.log('ip', ip);
+    });
+  }
+
+  useEffect(() => {
+    setMounted(true)
+  }, []);
  
   return (
     <div className="app">
